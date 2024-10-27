d01003edcd4dcde40053765d657e3f07c1531318
kubickipi314 <kubicki.pi314@gmail.com>
2024-10-27 21:56:01 +0100

    streams

--- src/Lambda_stream/Lambda_lambda/Main.java
+++ src/Lambda_stream/Lambda_lambda/Main.java
@@ -1,8 +1,32 @@
 package Lambda_stream.Lambda_lambda;
 
+import java.util.function.Consumer;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        double a = 10.0;
+        double b = 5.0;
+
+        System.out.println("\nAll Operations:");
+        for (Operation op : Operation.values()) {
+            System.out.println(a + " " + op + " " + b + " = " + op.apply(a, b));
+        }
+
+
+        ResourceRunnable rr = new ResourceRunnable();
+        Runnable lambda = rr.getRunnable();
+        lambda.run();
+        rr.resource = "NewResource";
+        lambda.run();
+
+        System.out.print('\n');
+
+        MyEventProducer eventProducer = new MyEventProducer();
+        EventConsumerImpl consumer = new EventConsumerImpl();
+
+        consumer.attach(eventProducer);
+
+        eventProducer.triggerEvent("TestEvent");
     }
 }
