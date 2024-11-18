39b2bacbeec3a4e7721ab318724e03bf4c459940
Tomasz Otręba <107582760+thaitoos@users.noreply.github.com>
2024-11-18 10:40:26 +0100

    Create Proc3.java

---
+++ src/annotations/Proc3.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+@SupportedAnnotationTypes("org.example.Immutable")
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+public class ImmutableAnnotationProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv)
+    {
+        for (final Element element :
+                roundEnv.getElementsAnnotatedWith(Immutable.class)) {
+            if (element instanceof TypeElement) {
+                final TypeElement typeElement = (TypeElement)element;
+
+                for (final Element eclosedElement :
+                        typeElement.getEnclosedElements()) {
+                    if (eclosedElement instanceof VariableElement) {
+                        final VariableElement variableElement =
+                                (VariableElement)eclosedElement;
+                        if( !variableElement.getModifiers().contains( Modifier.FINAL ) )
+                            processingEnv.getMessager().printMessage(
+                                    Diagnostic.Kind.ERROR,
+                                    String.format( "Class '%s' is annotated as @Immutable, " +
+                                                    "but field '%s' is not declared as final",
+                                            typeElement.getSimpleName(),
+                                            variableElement.getSimpleName()
+                                    )
+                            );
+                    }
+                }
+            }
+        }
+        // Claim that annotations have been processed by this processor
+        return true;
+    }
+}
